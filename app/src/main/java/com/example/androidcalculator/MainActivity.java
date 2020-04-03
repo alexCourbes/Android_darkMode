949eba6648342bb6b23420935c8b3d31f061222e
alexCourbes <courbesalex@gmail.com>
2020-04-03 02:12:32 +0100

    version 1.0 for android application

---
+++ app/src/main/java/com/example/androidcalculator/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.androidcalculator;
+
+import android.graphics.Color;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    //global variable
+     static boolean check = true;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState); //preload stuff
+        setContentView(R.layout.activity_main); //prewritten
+
+        //local variable delcaration
+        final  Switch darkModeSwitch = findViewById(R.id.switchToggle);
+        TextView Txt = findViewById(R.id.textView);
+
+
+        darkModeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if (check == true){
+                    dark();
+                } else {
+                    light();
+                }
+            }
+        });
+
+
+
+    }
+
+    public void dark(){
+        View view = this.getWindow().getDecorView();
+        view.setBackgroundColor(Color.parseColor ("#121212"));
+        TextView Txt = findViewById(R.id.textView);
+        Txt.setTextColor(Color.parseColor("#BB86FC"));
+        check = false;
+    }
+
+    public void light(){
+        View view = this.getWindow().getDecorView();
+        view.setBackgroundColor(Color.parseColor ("#FFFFFF"));
+        TextView Txt = findViewById(R.id.textView);
+        Txt.setTextColor(Color.parseColor("#000000"));
+        check = true;
+    }
+
+
+
+}
